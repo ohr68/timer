19541b6f0f45ff5d3c6033e61284eb5f364385cb
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-11 21:20:10 -0300

    feat: :sparkles: Adiciona tela Home e tela de Histórico

---
+++ src/interfaces/StatusProps.ts
@@ -0,0 +1,3 @@
+export interface StatusProps {
+  statusColor: 'yellow' | 'red' | 'green'
+}
---
+++ src/pages/History/index.tsx
@@ -0,0 +1,64 @@
+import { HistoryContainer, HistroryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistroryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistroryList>
+    </HistoryContainer>
+  )
+}
