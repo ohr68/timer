ba5b55f4b5cc29fa78337099fc24ed86d8e580e2
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-14 20:37:52 -0300

    refactor: :recycle: Adiciona a biblioteca immer para melhorar a estrutura do reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,22 +1,44 @@
-import { createContext, useReducer, useState } from 'react'
+import { createContext, useEffect, useReducer, useState } from 'react'
 import type { CyclesContextType } from '../interfaces/CyclesContextType'
 import type { Cycle } from '../interfaces/Cycle'
 import type { CreateCycleData } from '../interfaces/CreateCycleData'
 import type { CyclesContextProviderProps } from '../interfaces/CyclesContextProviderProps'
 import { cyclesReducer } from '../reducers/cycles/reducer'
-import { addNewCycleAction } from '../reducers/cycles/actions'
+import {
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions'
 
 export const CyclesContext = createContext({} as CyclesContextType)
 
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJson = localStorage.getItem(
+        '@timer:cycles-state-1.0.0',
+      )
+
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson)
+      }
+    },
+  )
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  useEffect(() => {
+    const stateJson = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@timer:cycles-state-1.0.0', stateJson)
+  }, [cyclesState])
 
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
@@ -26,7 +48,7 @@ export function CyclesContextProvider({
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch(markCurrentCycleAsFinished())
+    dispatch(markCurrentCycleAsFinishedAction())
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -44,7 +66,7 @@ export function CyclesContextProvider({
   }
 
   function interruptCurrentCycle() {
-    dispatch(interruptCurrentCycle())
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -5,7 +5,7 @@ export function addNewCycleAction(newCycle: Cycle) {
   return {
     type: ActionTypes.ADD_NEW_CYCLE,
     payload: {
-      data: newCycle,
+      newCycle,
     },
   }
 }
@@ -16,7 +16,7 @@ export function interruptCurrentCycleAction() {
   }
 }
 
-export function markCycleAsFinished() {
+export function markCurrentCycleAsFinishedAction() {
   return {
     type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
   }
